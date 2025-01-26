b45be40a5a1c310036aa0cda470f36dabccd5e38
SL6I <s443004498@st.uqu.edu.sa>
2025-01-26 19:38:24 +0300

    date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,18 @@
+<script lang = 'ts' >
+import dayjs from "dayjs"
+ import 'dayjs/locale/ar'
+ dayjs.locale('ar') 
+ let prd = dayjs().format('a') == 'م'? "مساء" : "صباح"
+</script>
+
 <div class="p-16">
   <header class="flex justify-between">
     <div>
-      <h1 class="text-4xl mb-2">صباح الخير 👋</h1>
-      <h2 class="text-2xl text-surface-900/50">اليوم الأحد 26</h2>
+      <h1 class="text-4xl mb-2">{prd} الخير 👋</h1>
+      <h2 class="text-2xl text-surface-900/50">
+	{dayjs().locale('ar').format('اليوم, dddd D MMM YYYY')}
+	</h2>
     </div>
-    <div class="bg-red-200 w-32 h-32"></div>
+    <div class="bg-white w-32 h-16"></div>
   </header>
 </div>
