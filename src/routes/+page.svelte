f9b2c00c49409c656cc77a48b3cccb0b6ba60896
SL6I <s443004498@st.uqu.edu.sa>
2025-01-26 19:03:50 +0300

    Header

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="p-16">
+  <header class="flex justify-between">
+    <div>
+      <h1 class="text-4xl mb-2">صباح الخير 👋</h1>
+      <h2 class="text-2xl text-surface-900/50">اليوم الأحد 26</h2>
+    </div>
+    <div class="bg-red-200 w-32 h-32"></div>
+  </header>
+</div>
