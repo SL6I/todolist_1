95922ec4227fb93c703145016efd76f4b6d6f835
SL6I <s443004498@st.uqu.edu.sa>
2025-02-01 05:05:38 +0300

    cONVERT

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
-import Header from "$lib/Header.svelte"
-
-
-</script>
-
-<div class="p-16">
-<Header />
-</div>
+    import Header from "$lib/Header.svelte"
+      import { svelte } from "@sveltejs/vite-plugin-svelte";
+      import { title } from "process";
+    
+    
+    </script>
+    <svelte:head> 
+    <title>
+        تيقن
+    </title>
+    </svelte:head>
+    <div class="p-16">
+    <Header />
+    </div>
+    
